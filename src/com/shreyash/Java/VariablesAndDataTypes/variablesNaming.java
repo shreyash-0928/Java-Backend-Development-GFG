b147925fd3441d881bc0de0ca5880652c15d5301
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2024-01-03 17:31:17 +0530

    variables Naming

---
+++ src/com/shreyash/Java/VariablesAndDataTypes/variablesNaming.java
@@ -0,0 +1,13 @@
+package com.shreyash.Java.VariablesAndDataTypes;
+
+public class variablesNaming {
+    //Valid names = ab, AB, aBC, a1, a12, _a, a_b, ab$, $ab...
+
+    //Invalid Name = 11a, #a, a!, else, do...
+
+    // Java Uses Camal case
+
+    //Constants = MAX_AGE // final keyword is use for constants in java
+
+
+}
