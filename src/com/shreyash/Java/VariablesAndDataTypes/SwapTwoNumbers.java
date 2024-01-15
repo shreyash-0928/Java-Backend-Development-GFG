56a2d15419d90fe696ced42aa3e497de21c88a11
Shreyash Vidhate <shreyashvidhate88@gmail.com>
2024-01-15 21:18:17 +0530

    Swap Two Numbers

---
+++ src/com/shreyash/Java/VariablesAndDataTypes/SwapTwoNumbers.java
@@ -0,0 +1,16 @@
+package com.shreyash.Java.VariablesAndDataTypes;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int temp = 0;
+        temp = a;
+        a=b;
+        b=temp;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
